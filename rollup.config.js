94a271058df563c53b180c35cf582f788c077acc
dev-maeconomy <dev@maeconomy.org>
2026-01-06 10:52:58 +0200

    Update/replace packages

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
 import { readFileSync } from 'fs';
